5ae4b3ae8a05ded4413d441e599a2ebff9ecf0c1
nmahdi6 <nmahdi600@gmail.com>
2022-04-21 16:40:10 +0430

    travel app project version 1

---
+++ lib/Model/TravelModel.dart
@@ -0,0 +1,29 @@
+class TravelModel{
+
+String name;
+String location;
+String image;
+String distance;
+String temp;
+String rating;
+String discription;
+String price;
+
+TravelModel({
+  required this.name,
+  required this.location,
+  required this.image,
+  required this.distance,
+  required this.temp,
+  required this.rating,
+  required this.discription,
+  required this.price
+});
+}
+
+List<TravelModel> travelList =[
+  TravelModel(name: "New York", location: "US - New York", image: "assets/images/NewYork.jpg", distance: "6", temp: "4.6", rating: "5", discription: "New York City: Reborn is a wide-ranging Manhattan Institute project that will encompass research, journalism, and event programming. The institute, through this initiative, convenes business, civic, and academic leaders from around the city with MI scholars to discuss key issues—including public transit, housing, the city budget, infrastructure, education, public safety, and small-business competitiveness. Participants join issue-specific working groups, led by MI scholars, to develop innovative policy ideas and draft a series of issue briefs. This research will ultimately form a comprehensive agenda for the city’s recovery and a policy blueprint for its future leaders.", price: "1950"),
+  TravelModel(name: "Tehran", location: "Iran - Tehran", image: "assets/images/Tehran.jpg", distance: "7", temp: "3.6", rating: "4", discription: "Tehran is the capital of Iran and one of the largest and most populous cities in the world, with a population of 14 million. Tehran has a temperate climate, as it is located at 35.41 north latitude and 51.15 east longitude and 1,200 meters above sea level , Which makes it a little cooler than other capitals in the Middle East. However, this does not prevent escaping Tehran's hot summer and its cold winter for a nice. The best time to visit Tehran is late September to early November and mid-April to early June.", price: "1800"),
+  TravelModel(name: "Paris", location: "France - Paris", image: "assets/images/Paris.jpg", distance: "10", temp: "4.8", rating: "4.8", discription: "Paris is monumental, both due to its size but also its wealth of architectural heritage! The symbol of the capital city, and doubtless its best-known monument, is the unique Eiffel Tower, the 'Iron Lady' that offers visitors an unsurpassed view. Close by is the equally iconic Arc de Triomphe. This historical landmark dazzles onlookers on summer days, when the sun sets through the huge arch. Notre Dame Cathedral, a masterpiece of Gothic architecture, is another magnificent and unmissable sight, where you can admire sculptures, the nave, towers and even the crypt! Just as evocative, the Sacré-Cœur Basilica has been the source of inspiration for writers and film-makers. It houses the largest mosaic in France and boasts an incredible panoramic view over the city from the top of the Montmartre district! Paris is also a city of iconic squares, including the Place de la Concorde with its two fountains, rostral columns and the incredible Luxor Obelisk, the Place de la Bastille, with its famous July Column commemorating the revolution and its very modern Paris National Opera and the Place de l'Opéra, featuring the magnificent Opéra Garnier.", price: "2106"),
+  TravelModel(name: "Roma", location: "Italy - Rome", image: "assets/images/Roma.jpg", distance: "16", temp: "6.1", rating: "4.2", discription: "Situated on the river Tiber, between the Apennine mountains and the Tyrrhenian Sea, the 'Eternal City' was once the administrative centre of the mighty Roman Empire, ruling over a vast territory that stretched all the way from Britain to Mesopotamia. Today, the city is the seat of the Italian government and home to numerous ministerial offices. Rome has 2.6 million inhabitants while its metropolitan area is home to around 4.2 million.", price: "1678"),
+];
---
+++ lib/main.dart
@@ -0,0 +1,279 @@
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:travel_app/Model/TravelModel.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage  extends StatefulWidget {
+  const MainPage ({ Key? key }) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+  double imageSize = 55;
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+   
+    return Scaffold(
+      // safeArea 
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+              width: double.infinity,
+              height: size.height/1.9,
+              
+              child: Stack(
+                children: [
+                  // background image
+                  Container(
+                    width: double.infinity,
+                    height: size.height/2.3,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(70),
+                        bottomRight: Radius.circular(70),
+                        ),
+                      image: DecorationImage(
+                        image: AssetImage(travelList[_selectedIndex].image),
+                        fit: BoxFit.cover,
+                        )
+                    ),
+                  ),
+        
+                  // head icons
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    child: Padding(
+                    padding: const EdgeInsets.fromLTRB(10,20,10,0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: const BoxDecoration(color: Color.fromARGB(100, 255, 255, 255),shape: BoxShape.circle),
+                          child: const Icon(CupertinoIcons.arrow_left),
+                        ), 
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: const BoxDecoration(color: Color.fromARGB(100, 255, 255, 255),shape: BoxShape.circle),
+                          child: const Icon(CupertinoIcons.heart),
+                        ),
+                      ],
+                    ),
+                              ),
+                  ),
+                  
+                  //image list
+                  Positioned(
+                    right: 0,
+                    top: 106,
+                    child: Container(
+                      height: size.height/3,
+                      width: imageSize*2,
+                      child: SizedBox(
+                        child: ListView.builder(
+                          itemCount: travelList.length,
+                          itemBuilder:(context, index) {
+                            return imageItem(index);
+                        },),
+                      ),
+                    ),
+                  ),
+        
+                  //name & place
+                  Positioned(
+                    top: size.height/2.9,
+                    left: size.width/10,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(travelList[_selectedIndex].name,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+                        SizedBox(height: 8,),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.placemark_fill,color: Colors.white,),
+                            Text(travelList[_selectedIndex].location,style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal,fontSize: 16)),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+                ),
+              
+            ),
+            Container(
+              width: double.infinity,
+        
+              child: Column(
+                children: [
+                  //3 of box
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: const BorderSide(color: Color.fromARGB(40, 0, 0, 0)) 
+                          ),
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Distance",style: TextStyle(fontWeight: FontWeight.bold),),
+                                Text(travelList[_selectedIndex].distance + " Km "),
+                              ],
+                            ),
+                          ),
+                      ),
+                      Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: const BorderSide(color: Color.fromARGB(40, 0, 0, 0)) 
+                          ),
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Temp",style: TextStyle(fontWeight: FontWeight.bold),),
+                                Text(travelList[_selectedIndex].temp + " \u00B0C "),
+                              ],
+                            ),
+                          ),
+                      ),
+                      Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          side: const BorderSide(color: Color.fromARGB(40, 0, 0, 0)) 
+                          ),
+                          child: Container(
+                            width: 90,
+                            height: 90,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Text("Rating",style: TextStyle(fontWeight: FontWeight.bold),),
+                                Text(travelList[_selectedIndex].rating),
+                              ],
+                            ),
+                          ),
+                      ),
+                      
+                    ],
+                  ),
+                  //description
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(24 , 16 , 24 , 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                      const Text("Description",
+                      style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+                      const SizedBox(height: 8,),
+                      ExpandableText(
+                        travelList[_selectedIndex].discription,
+                        expandText: "Read more",
+                        maxLines: 3,
+                        collapseText: "Collapse",
+                      ),
+                  //sizedbox
+                  SizedBox(height: 16,),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children:  [
+                      const Text("Total Price",style: TextStyle(fontWeight: FontWeight.normal),),
+                      Text(travelList[_selectedIndex].price + "\$", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),)
+                    ],),
+
+                    Container(
+                          width: 96,
+                          height: 96,
+                          decoration: const BoxDecoration(color: Color.fromARGB(250, 0, 0, 0),shape: BoxShape.circle),
+                          child: const Icon(CupertinoIcons.arrow_right, color: Colors.white,),
+                        ),
+                  ],)
+
+
+                    ],),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget imageItem(int index) {
+    return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap:() {
+                            setState(() {
+                            _selectedIndex = index;
+                            }); 
+                          },
+                          
+                          child: AnimatedContainer(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: _selectedIndex==index? Colors.yellow : Colors.white ,width: 2.2),
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(image: AssetImage(travelList[index].image),
+                              fit: BoxFit.fill)
+                            ),
+                            width: _selectedIndex==index? imageSize + 15 : imageSize,
+                            height: _selectedIndex==index? imageSize + 15 : imageSize,
+                            
+                            
+                            duration: const Duration(milliseconds: 500),
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+  }
+} 
+
+
+
